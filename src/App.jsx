279d81842d762dca334f0fbe7c91f2b7762347ab
zaenudinputra44 <zaenudinputra44@gmail.com>
2026-06-29 16:55:36 +0700

    Fix: Resolve route mismatch for Management Aset to fix blank page

--- src/App.jsx
+++ src/App.jsx
@@ -130,7 +130,7 @@ const AppRoutes = () => {
               <Route path="/performance" element={<Performance />} />
               <Route path="/winning" element={<Winning />} />
               <Route path="/technical" element={<Technical />} />
-              <Route path="/assets" element={<Aset />} />
+              <Route path="/aset" element={<Aset />} />
               <Route path="/evaluation" element={<Evaluation />} />
               <Route path="/kol" element={<KOL />} />
               <Route path="/settings" element={<Settings />} />
